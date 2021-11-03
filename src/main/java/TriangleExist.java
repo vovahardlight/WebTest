f6888dacbabb4cfe5539b642c08c4943d8d634bc
Admin <Admin@KYVP39OFF15WS21.uven.loc>
2021-11-04 01:04:46 +0200

    Initial commit

---
+++ src/main/java/TriangleExist.java
@@ -0,0 +1,14 @@
+public class TriangleExist {
+
+
+    public static void main(String[] args) {
+        int a = -2;
+        int b = 3;
+        int c = 4;
+        if (a > 0 && b > 0 && c > 0)
+            System.out.println("Triangle exists");
+         else {
+            System.out.println("Triangle not exists");
+        }
+    }
+}
